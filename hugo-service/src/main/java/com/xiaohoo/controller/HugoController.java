79472433a2d32ab559d1a0f2f838a44cd1fbc0f1
yangxiaohu <1126457667@qq.com>
2020-06-17 10:06:24 +0800

    init

---
+++ hugo-service/src/main/java/com/xiaohoo/controller/HugoController.java
@@ -0,0 +1,31 @@
+package com.xiaohoo.controller;
+
+import io.swagger.annotations.Api;
+import me.zhengjie.annotation.AnonymousAccess;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ * @author ：yangxiaohu
+ * @date ：2020/6/12
+ * @email :1126457667@qq.com
+ */
+@RestController
+@RequestMapping("/hugo")
+@Api(tags = "测试：本地化测试")
+public class HugoController {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @GetMapping("/hi")
+    @AnonymousAccess
+    public String test() {
+        return "hi";
+    }
+}
